3dbfe87ec3d929cd0c4dccb52a8ca23e20e887a9
pal_user <pentivinu@gmail.com>
2020-01-29 11:00:42 -0700

    Database creation

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class PalTrackerApplication {
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
         SpringApplication.run(PalTrackerApplication.class, args);
     }
 
